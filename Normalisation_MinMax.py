d49919452bcae937356100966734e57fd4f72adc
Romain Veillard <romain.veillard@gearset.com>
2024-10-30 20:54:39 +0000

    Compute min-max amongst all events instead of single file

--- Normalisation_MinMax.py
+++ Normalisation_MinMax.py
@@ -10,16 +10,14 @@ normalisation_channel = '165Ho_AlexaFluor488'
 
 columns_to_ignore = ['Cell_Index', normalisation_channel]
 
-def normalise_file(filename):
+def normalise_file(filename, min_max_norm):
     file_path = os.path.join(folder_path, filename)
 
     # Read the file
     events = pd.read_csv(file_path, delimiter='\t')
 
     # X-Normalise the channel (Min-max normalization)
-    data_min = events[normalisation_channel].min()
-    data_max = events[normalisation_channel].max()
-    events[normalisation_channel] = events[normalisation_channel].map(lambda x: (x - data_min) / (data_max - data_min))
+    events[normalisation_channel] = events[normalisation_channel].map(min_max_norm)
 
     # Separate the columns to normalize and the columns to leave as-is
     columns_to_normalize = events.columns.difference(columns_to_ignore)
@@ -39,15 +37,32 @@ def normalise_file(filename):
 
     print(f"Normalised file: {file_path}")
 
+def get_min_max_function():
+    all_events = pd.DataFrame()
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(folder_path, filename)
+            # Load the file
+            events = pd.read_csv(file_path, delimiter='\t')
+            # Add the data to the overall DataFrame
+            all_events = pd.concat([all_events, events], ignore_index=True)
+    
+    data_min = all_events[normalisation_channel].min()
+    data_max = all_events[normalisation_channel].max()
+
+    return lambda x: (x - data_min) / (data_max - data_min)
 
 # Create the normalised folder
 normalised_folder_path = os.path.join(folder_path, "normalised")
 if not os.path.exists(normalised_folder_path):
     os.makedirs(normalised_folder_path)
 
+min_max_norm = get_min_max_function()
+
 # Loop through each file in the folder
 for filename in os.listdir(folder_path):
     if filename.endswith('.txt'):
         # Normalise the file
-        normalise_file(filename)
+        normalise_file(filename, min_max_norm)
         
